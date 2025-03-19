fe3fdbb9951a649de6c7160cdfbf2cbedfda8fb1
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-03-19 18:23:39 +0100

    working environment

--- Trace.Tests/ColorTests.cs
+++ Trace.Tests/ColorTests.cs
@@ -1,4 +1,56 @@
-/* 
+
+namespace Trace.Tests;
+
+public class ColorTests
+{
+    [Fact]
+    public void TestColorAddition()
+    {
+        var color1 = new Color(5.0f, 6.0f, 7.0f);
+        var color2 = new Color(2.0f, 3.0f, 4.0f);
+        var expected = new Color(7.0f, 9.0f, 11.0f);
+
+        var result = color1 + color2;
+        
+        Assert.True(Color.are_close_colors(expected, result), $"ColorAddition doesn't work properly.");
+    }
+
+    [Fact]
+    public void TestColorMultiplication()
+    {
+        var color1 = new Color(5.0f, 6.0f, 7.0f);
+        var color2 = new Color(2.0f, 3.0f, 4.0f);
+        var expected = new Color(10.0f, 18.0f, 28.0f);
+
+        var result = color1 * color2;
+        
+        Assert.True(Color.are_close_colors(expected, result), $"ColorMultiplication doesn't work properly");
+    }
+
+    [Fact]
+    public void TestColorScalarMultiplication()
+    {
+        var color1 = new Color(5.0f, 6.0f, 7.0f);
+        const float scalar = 2.0f;
+        var expected = new Color(10.0f, 12.0f, 14.0f);
+
+        var result = scalar*color1;
+        
+        Assert.True(Color.are_close_colors(expected, result), $"ColorScalarMultiplication doesn't work properly.");
+    }
+
+    [Fact]
+    public void TestColorCloseness()
+    {
+        var color1 = new Color(5.0f, 6.0f, 7.0f);
+        var color3 = new Color(5.0f, 6.0f, 7.0f);
+        var result = Color.are_close_colors(color1, color3);
+
+        Assert.True(result, $"ColorCloseness doesn't work properly.");
+    }
+}
+
+/*
 using System.Reflection.Metadata;
 
 namespace Trace;
@@ -33,57 +85,6 @@ class Program
 }
 
 
-namespace Trace.Tests;
-
-public class ColorTests
-{
-    [Fact]
-    public void TestColorAddition()
-    {
-        var color1 = new Color(5.0f, 6.0f, 7.0f);
-        var color2 = new Color(2.0f, 3.0f, 4.0f);
-        var expected = new Color(7.0f, 9.0f, 11.0f);
-
-        var result = color1 + color2;
-        
-        Assert.True(Color.are_close_colors(expected, result));
-    }
-
-    [Fact]
-    public void TestColorMultiplication()
-    {
-        var color1 = new Color(5.0f, 6.0f, 7.0f);
-        var color2 = new Color(2.0f, 3.0f, 4.0f);
-        var expected = new Color(10.0f, 18.0f, 28.0f);
-
-        var result = color1 + color2;
-        
-        Assert.True(Color.are_close_colors(expected, result));
-    }
-
-    [Fact]
-    public void TestColorScalarMultiplication()
-    {
-        var color1 = new Color(5.0f, 6.0f, 7.0f);
-        const float scalar = 2.0f;
-        var expected = new Color(10.0f, 12.0f, 14.0f);
-
-        var result = scalar*color1;
-        
-        Assert.True(Color.are_close_colors(expected, result));
-    }
-
-    [Fact]
-    public void TestColorCloseness()
-    {
-        var color1 = new Color(5.0f, 6.0f, 7.0f);
-        var color3 = new Color(5.0f, 6.0f, 7.0f);
-        var result = Color.are_close_colors(color1, color3);
-
-        Assert.True(result);
-    }
-}
-*/
 using System;
 using Xunit;
 
@@ -103,5 +104,5 @@ namespace Trace.Tests
     }
 }
 
-
+*/
 

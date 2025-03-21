665ce2ee9ecec3e0991b77f74b1c9c265349f7d2
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-03-21 09:03:11 +0100

    implement test granularity in HdrImage.cs

--- Trace/HdrImage.cs
+++ Trace/HdrImage.cs
@@ -1,3 +1,5 @@
+using Xunit;
+
 namespace Trace;
 
 public class HdrImage
@@ -5,7 +7,7 @@ public class HdrImage
     public int Width { get; set; }
     public int Height { get; set; }
     public Color[] Pixels { get; set; }
-
+    
     // Constructor that initializes the image with a width and height
     public HdrImage(int width = 0, int height = 0)
     {
@@ -19,41 +21,37 @@ public class HdrImage
             Pixels[i] = new Color();
         }
     }
-    
-    public Color GetPixel(int x, int y)
+
+    private bool valid_coordinates(int x, int y)
     {
         // Check if x is out of bounds
         if (x < 0 || x >= Width)
         {
             throw new ArgumentOutOfRangeException(nameof(x), "The x coordinate is out of bounds.");
         }
-
         // Check if y is out of bounds
         if (y < 0 || y >= Height)
         {
             throw new ArgumentOutOfRangeException(nameof(y), "The y coordinate is out of bounds.");
-        }
-
+        }        
+        return x < Width && y < Height;
+    }
+    public int pixel_offset(int x, int y)
+    {
+        return y * Width + x;
+    }    
+    public Color GetPixel(int x, int y)
+    {
+        Assert.True(valid_coordinates(x, y));
         // Return the color of the pixel
-        return Pixels[y * Width + x];
+        return Pixels[pixel_offset(x, y)];
     }
 
     public void SetPixel(int x, int y, Color newColor)
     {
-        // Check if x is out of bounds
-        if (x < 0 || x >= Width)
-        {
-            throw new ArgumentOutOfRangeException(nameof(x), "The x coordinate is out of bounds.");
-        }
-
-        // Check if y is out of bounds
-        if (y < 0 || y >= Height)
-        {
-            throw new ArgumentOutOfRangeException(nameof(y), "The y coordinate is out of bounds.");
-        }
-
+        Assert.True(valid_coordinates(x, y));
         // Set the new color for the pixel
-        Pixels[y * Width + x] = newColor;
+        Pixels[pixel_offset(x,y)] = newColor;
     }
 
 }

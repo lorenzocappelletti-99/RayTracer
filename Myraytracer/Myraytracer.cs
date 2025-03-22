1106b02ca1a977ed81788583af6513f6d3e2788b
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-03-22 11:17:37 +0100

    1st implement writePFM

--- Myraytracer/Myraytracer.cs
+++ Myraytracer/Myraytracer.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Trace;
+using System;
+
+
 Console.WriteLine("Hello, World!");
+
+var img = new HdrImage(3,10);
+
+img.WritePfm(img);
--- Trace/HdrImage.cs
+++ Trace/HdrImage.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using System.Text;
 
 namespace Trace;
 
@@ -55,5 +56,88 @@ public class HdrImage
         // Set the new color for the pixel
         Pixels[pixel_offset(x,y)] = newColor;
     }
+    
+    
+    /*
+ * def _write_float(stream, value):
+# Meaning of "<f":
+# "<": little endian
+# "f": single-precision floating point value (32 bit)
+stream.write(struct.pack("<f", value))
+ */
+    private static void WriteFloat(Stream outputStream, float value, bool littleEndian = false)
+    {
+        var bytes = BitConverter.GetBytes(value);
+
+        // Se la macchina è little-endian ma vogliamo big-endian, invertiamo i byte
+        if (BitConverter.IsLittleEndian != littleEndian)
+        {
+            Array.Reverse(bytes);
+        }
+
+        outputStream.Write(bytes, 0, bytes.Length);
+    }
+
+    public void SavePfm(Stream outputStream)
+    {
+        
+    }
+
+    public void WritePfm(HdrImage img, bool endianness = false)//(, int endianness=Endianness.LITTLE_ENDIAN)
+    {
+        string filePath = @"C:\Users\pietr\OneDrive\Desktop\fisica\magistrale\SEM_2\FotoReal\Myraytracer\Myraytracer\output.pfm";
+
+        using FileStream fileStream = File.OpenWrite(filePath);
+        using BinaryWriter writer = new BinaryWriter(fileStream, Encoding.ASCII);
+
+        // Header
+        writer.Write(Encoding.ASCII.GetBytes("PF\n")); // "PF" indica un'immagine RGB
+        writer.Write(Encoding.ASCII.GetBytes($"{img.Width} {img.Height}\n"));
+
+        var floatEndiannes = -1.0f;
+        if (endianness == true){ floatEndiannes = 1.0f; }
+        writer.Write(Encoding.ASCII.GetBytes($"{floatEndiannes}\n"));
+
+        for (int y = 0; y < img.Height; y++)
+        {
+            for (int x = 0; x < img.Width; x++)
+            {
+                var color = this.GetPixel(x, y);
+                WriteFloat(fileStream, color.R);
+                WriteFloat(fileStream, color.G);
+                WriteFloat(fileStream, color.B);
+            }
+        }
+        
+        /*
+         *     # Write the image (bottom-to-up, left-to-right)
+    for y in reversed(range(self.height)):
+        for x in range(self.width):
+            color = self.get_pixel(x, y)
+            _write_float(stream, color.r, endianness)
+            _write_float(stream, color.g, endianness)
+            _write_float(stream, color.b, endianness)
+         */
+    }
+
+    /*
+    def write_pfm(self, 
+    ):
+    endianness_str = "-1.0" if endianness == Endianness.LITTLE_ENDIAN else "1.0"
+
+    # The PFM header, as a Python string (UTF-8)
+    header = f"PF\n{self.width} {self.height}\n{endianness_str}\n"
+
+    # Convert the header into a sequence of bytes
+    stream.write(header.encode("ascii"))
+
+    # Write the image (bottom-to-up, left-to-right)
+    for y in reversed(range(self.height)):
+        for x in range(self.width):
+            color = self.get_pixel(x, y)
+            _write_float(stream, color.r, endianness)
+            _write_float(stream, color.g, endianness)
+            _write_float(stream, color.b, endianness)
+     */
 
 }

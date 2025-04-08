c3cc26ad663f7261eb72124b4f91bfdd48d3b6ad
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-04-08 22:14:43 +0200

    implement first Transformation.cs struct functionalities

---
+++ Trace/Transformation.cs
@@ -0,0 +1,55 @@
+/*===========================================================
+ |                     Raytracer Project                    
+ |             Released under EUPL-1.2 License               
+ |                       See LICENSE                        
+ ===========================================================*/
+
+
+namespace Trace;
+
+
+public struct Transformation(float[,]? m = null, float[,]? invm = null)
+{
+    private static readonly float[,] IdentityMatr4X4 = new float[, ]
+    {
+        { 1.0f, 0.0f, 0.0f, 0.0f },
+        { 0.0f, 1.0f, 0.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 0.0f, 1.0f }
+    };
+
+    public float[,] M = m ?? IdentityMatr4X4;
+    public float[,] Invm = invm ?? IdentityMatr4X4;
+
+    public static bool AreClose(Transformation v1, Transformation v2, float sigma = 1e-5f)
+    {
+        for (var i = 0; i < 4; i++)
+        {
+            for (var j = 0; j < 4; j++)
+            {
+                if (Color.are_close(v1.M[i, j], v2.M[i, j], sigma)) return false;
+            }
+        }
+        return true;
+    }
+
+    Transformation _matr_prod(Transformation a, Transformation b)
+    {
+        var result = new Transformation();
+        for (var i = 0; i < 4; i++)
+        {
+            for (var j = 0; j < 4; j++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    if (result.M != null) result.M[i, j] += a.M[i, k] * b.M[k, j];
+                }
+            }
+        }
+        return result;
+    }
+    
+    
+    
+}
+

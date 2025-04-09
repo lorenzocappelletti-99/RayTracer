11f5270c6f913a1aa3923e79674bf1e6d3a2d9a3
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-04-09 12:10:44 +0200

    implement first features form Camera.cs ImageTracer.cs Ray.cs and first tests
    # Conflicts:
    #	Trace/Transformation.cs

---
+++ Trace.Tests/TestTransform.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace Trace.Tests;
+/*
+public class TestTransform
+{
+    [Fact]
+    public void TestTransformation()
+    {
+        
+        var ray = new Ray(origin: new Point(1.0f, 2.0f, 3.0f), direction: new Vec(6.0f, 5.0f, 4.0f));
+        var transformation = Transformation.translation(new Vec(10.0, 11.0, 12.0)) * Transformation.rotation_x(90.0);
+        var transformed = ray.transform(transformation);
+
+    }
+    
+}
+*/
---
+++ Trace/Ray.cs
@@ -0,0 +1,60 @@
+namespace Trace;
+
+public struct Ray
+{
+    public readonly Point Origin;
+    public readonly Vec Direction;
+    public readonly float Tmin = 1e-5f;
+    public readonly float Tmax = float.PositiveInfinity;
+    public readonly int Depth = 0;
+
+    public Ray(Point origin, Vec direction, float tmin, float tmax, int depth)
+    {
+        Origin = origin;
+        Direction = direction;
+        Tmin = tmin;
+        Tmax = tmax;
+    }
+    public Ray(Point origin, Vec direction)
+    {
+        Origin = origin;
+        Direction = direction;
+    }
+
+    /// <summary>
+    /// Check if two rays are similar enough to be considered equal
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="epsilon"></param>
+    /// <returns></returns>
+    public bool is_close(Ray b, float epsilon = 1e-5f)
+    {
+        return Point.AreClose(Origin, b.Origin) && Vec.AreClose(Direction, b.Direction);
+    }
+    
+    /// <summary>
+    /// Compute the point along the ray's path at some distance from the origin.
+    /// Return a ``Point`` object representing the point in 3D space whose distance from the
+    /// ray's origin is equal to `t`, measured in units of the length of `Vec.dir`.
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public Point PointAt(float t)
+    {
+        return Origin + Direction * t;
+    }
+    
+    
+/*
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="transform"></param>
+    /// <returns></returns>
+    public Ray Transform(Transformation transform)
+    {
+        return Ray(origin: transform * Origin, direction: transform * Direction);
+    }
+    */
+    
+}

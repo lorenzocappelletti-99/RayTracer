a157b39ec68ec5a8c262520090e863531d33bcde
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-04-30 12:26:39 +0200

    add new branch shape and file Shapes.cs

---
+++ Trace/Shapes.cs
@@ -0,0 +1,21 @@
+/*===========================================================
+ |                     Raytracer Project
+ |             Released under EUPL-1.2 License
+ |                       See LICENSE
+ ===========================================================*/
+
+namespace Trace;
+
+public class Shape
+{   
+    
+}
+
+/*class Shape:
+    def __init__(self, transformation=Transformation()):
+        self.transformation = transformation
+
+    def ray_intersection(self, ray: Ray) -> Optional[HitRecord]:
+        return NotImplementedError(
+            "Shape.ray_intersection is an abstract method and cannot be called directly"
+        )*/

f64aef62ef3cbea8db2df7a8b3db1e8c3b19126c
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-05-21 16:23:40 +0200

    Implement FlatRendererTest

--- Trace.Tests/RendererTest.cs
+++ Trace.Tests/RendererTest.cs
@@ -7,8 +7,14 @@
 using Xunit;
 namespace Trace.Tests;
 
+
+
 public class RendererTest
 {
+    private static readonly HdrImage Image = new HdrImage(width: 3, height: 3);
+    private static readonly OrthogonalProjection Camera = new OrthogonalProjection();
+    private readonly ImageTracer _tracer = new ImageTracer(Image, Camera);
+    
     [Fact]
     public void TestOnOffRenderer()
     {
@@ -22,73 +28,51 @@ public class RendererTest
             transformation: Transformation.Translation(new Vec(2, 0, 0)) *
                             Transformation.Scaling(new Vec(.2f, .2f, .2f)),
             material: whiteMaterial);
-        var image = new HdrImage(width: 3, height: 3);
-        var camera = new OrthogonalProjection();
-        var tracer = new ImageTracer(image, camera);
+        
         var world = new World();
         world.AddShape(sphere);
         var renderer = new OnOffRenderer(world);
-        tracer.FireAllRays(world, renderer.Render);
+        _tracer.FireAllRays(world, renderer.Render);
         
         
-        Assert.True(image.GetPixel(0, 0).IsClose(Color.Black));
-        Assert.True(image.GetPixel(1, 0).IsClose(Color.Black));
-        Assert.True(image.GetPixel(2, 0).IsClose(Color.Black));
-        Assert.True(image.GetPixel(0, 1).IsClose(Color.Black));
-        Assert.True(image.GetPixel(1, 1).IsClose(Color.White));
-        Assert.True(image.GetPixel(2, 1).IsClose(Color.Black));
-        Assert.True(image.GetPixel(0, 2).IsClose(Color.Black));
-        Assert.True(image.GetPixel(1, 2).IsClose(Color.Black));
-        Assert.True(image.GetPixel(2, 2).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(0, 0).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(1, 0).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(2, 0).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(0, 1).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(1, 1).IsClose(Color.White));
+        Assert.True(Image.GetPixel(2, 1).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(0, 2).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(1, 2).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(2, 2).IsClose(Color.Black));
+    }
+
+    [Fact]
+    public void TestFlatRenderer()
+    {
+        var sphereColor = new Color(1.0f, 2.0f, 3.0f);
+        var colMaterial = new Material
+        {
+            Pigment = new UniformPigment(new Color(1,2,3))  
+        };
+        var sphere = new Sphere(
+            radius: 0.1f,
+            transformation: Transformation.Translation(new Vec(2, 0, 0)) *
+                            Transformation.Scaling(new Vec(.2f, .2f, .2f)),
+            material: colMaterial);
+        var world = new World();
+        world.AddShape(sphere);
+        var renderer = new FlatRenderer(world);
+        _tracer.FireAllRays(world, renderer.Render);
+
+        Assert.True(Image.GetPixel(0, 0).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(1, 0).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(2, 0).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(0, 1).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(2, 1).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(0, 2).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(1, 2).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(2, 2).IsClose(Color.Black));
+        Assert.True(Image.GetPixel(1, 1).IsClose(sphereColor));
+        
     }
 }
-/*
- class TestRenderers(unittest.TestCase):
-    def testOnOffRenderer(self):
-        sphere = Sphere(transformation=translation(Vec(2, 0, 0)) * scaling(Vec(0.2, 0.2, 0.2)),
-                        material=Material(brdf=DiffuseBRDF(pigment=UniformPigment(WHITE))))
-        image = HdrImage(width=3, height=3)
-        camera = OrthogonalCamera()
-        tracer = ImageTracer(image=image, camera=camera)
-        world = World()
-        world.add_shape(sphere)
-        renderer = OnOffRenderer(world=world)
-        tracer.fire_all_rays(renderer)
-
-        assert image.get_pixel(0, 0).is_close(BLACK)
-        assert image.get_pixel(1, 0).is_close(BLACK)
-        assert image.get_pixel(2, 0).is_close(BLACK)
-
-        assert image.get_pixel(0, 1).is_close(BLACK)
-        assert image.get_pixel(1, 1).is_close(WHITE)
-        assert image.get_pixel(2, 1).is_close(BLACK)
-
-        assert image.get_pixel(0, 2).is_close(BLACK)
-        assert image.get_pixel(1, 2).is_close(BLACK)
-        assert image.get_pixel(2, 2).is_close(BLACK)
-
-    def testFlatRenderer(self):
-        sphere_color = Color(1.0, 2.0, 3.0)
-        sphere = Sphere(transformation=translation(Vec(2, 0, 0)) * scaling(Vec(0.2, 0.2, 0.2)),
-                        material=Material(brdf=DiffuseBRDF(pigment=UniformPigment(sphere_color))))
-        image = HdrImage(width=3, height=3)
-        camera = OrthogonalCamera()
-        tracer = ImageTracer(image=image, camera=camera)
-        world = World()
-        world.add_shape(sphere)
-        renderer = FlatRenderer(world=world)
-        tracer.fire_all_rays(renderer)
-
-        assert image.get_pixel(0, 0).is_close(BLACK)
-        assert image.get_pixel(1, 0).is_close(BLACK)
-        assert image.get_pixel(2, 0).is_close(BLACK)
-
-        assert image.get_pixel(0, 1).is_close(BLACK)
-        assert image.get_pixel(1, 1).is_close(sphere_color)
-        assert image.get_pixel(2, 1).is_close(BLACK)
-
-        assert image.get_pixel(0, 2).is_close(BLACK)
-        assert image.get_pixel(1, 2).is_close(BLACK)
-        assert image.get_pixel(2, 2).is_close(BLACK)
-
- */

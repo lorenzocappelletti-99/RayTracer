4d5a3efac36be2ccbdc611ac2d85bed3187dd111
pietro puppi <pietro.puppi1@studenti.unimi.it>
2025-05-28 11:55:24 +0200

    fixing same things

---
+++ Trace/SceneFile.cs
@@ -0,0 +1,19 @@
+namespace Trace;
+
+public class SourceLocation
+{
+    
+}
+
+/*class SourceLocation:
+    A specific position in a source file
+
+    This class has the following fields:
+    - file_name: the name of the file, or the empty string if there is no file associated with this location
+      (e.g., because the source code was provided as a memory stream, or through a network connection)
+    - line_num: number of the line (starting from 1)
+    - col_num: number of the column (starting from 1)
+    
+    file_name: str = ""
+    line_num: int = 0
+    col_num: int = 0*/
